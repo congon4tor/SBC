d6412af86502d4266eca186e39bcb8c413ccf051
Congon4tor <ignacio.dominguez95@gmail.com>
2018-01-16 17:43:18 +0100

    borrados vendors de la web que sobraban

--- SBCServer/app.py
+++ SBCServer/app.py
@@ -295,4 +295,4 @@ def exit_room():
         return jsonify(response = "fail", error="Ha ocurrido un error")
 
 if __name__ == '__main__':
-   app.run(debug=True,host= '0.0.0.0')
+   app.run(debug=True,host= '0.0.0.0',port=5000)
